5f94d61381d37554e6ccbd8da88f7fe6b5507d0c
defy82 <preetisahani@Preetis-MacBook-Pro-2.local>
2022-07-15 14:39:28 +0530

    initial commit

---
+++ src/main/java/com/scaler/tictactoe/Game.java
@@ -0,0 +1,6 @@
+package com.scaler.tictactoe;
+
+public class Game {
+
+
+}
---
+++ src/main/java/com/scaler/tictactoe/Player.java
@@ -0,0 +1,18 @@
+package com.scaler.tictactoe;
+
+public class Player {
+    private String character;
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+
+    public Player(String character) {
+        this.character = character;
+    }
+}
